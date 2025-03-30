10c10eeb6819785210ef4f1431b479f6f546ab57
Akash Jaiswar <jaiswarakash04@gmail.com>
2025-03-30 23:28:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,12 +20,5 @@
   scheduleNotifications();
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, async() => {
-      console.log(`Server running on port ${PORT}`)
-      try {
-          const url = await ngrok.connect(PORT);
-          console.log(`🌍 ngrok Public URL: ${url}`);
-          console.log(`🔗 Use this URL in your frontend: ${url}`);
-        } catch (err) {
-          console.error("❌ ngrok Error:", err);
-        }
+      console.log(`Server running on port ${PORT}`) 
   });
